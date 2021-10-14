2a821097e28c9dda24c104fa09316dbea278c94d
Yegor Bugayenko <yegor256@gmail.com>
2021-10-14 08:49:34 +0300

    pull script

---
+++ pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set +x
+set +e
+
+repo=$1
+tmp=$(mktemp -d)
+
+git clone "https://github.com/${1}" --branch gh-pages --depth 1 --single-branch "${tmp}"
+ls -al "${tmp}"
+cp -r "${tmp}/objectionary/*" .
