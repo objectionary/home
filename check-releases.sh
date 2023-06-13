c27f85c3c3bb9f3f2ae8c2ff89ec2c677b07db41
graur <ancongr@yandex.ru>
2023-06-13 19:29:45 +0300

    137 -fixed script

--- check-releases.sh
+++ check-releases.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-repositories=(
-    "eo-strings"
-    "eo-collections"
+declare -A repos=(
+  ["eo-strings"]="0.2.0"
+  ["eo-collections"]="0.0.9"
 )
 
-for repository in "${repositories[@]}"
-do
-    release=$(curl -s "https://api.github.com/repos/objectionary/$repository/releases/latest")
-    version=$(echo "$release" | jq -r '.tag_name')
-    echo "$repository latest release: $version"
+for repo in "${!repos[@]}"; do
+  latest_release=$(curl -s https://api.github.com/repos/objectionary/$repo/releases/latest | jq -r '.tag_name')
+
+  if [[ "$(echo -e "${repos[$repo]}n$latest_release" | sort -V | tail -n 1)" = "$latest_release" ]]; then
+    echo "Found new release for $repo: $latest_release"
+    ./pull.sh "objectionary/$repo"
+  else
+    echo "There is no new releases in $repo"
+  fi
 done
+
