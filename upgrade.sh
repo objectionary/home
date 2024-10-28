6381cbf00d5add70e4d098f0fecb1031cff28f11
maxonfjvipon <mtrunnikov@gmail.com>
2024-10-28 12:29:53 +0300

    Update EO to 0.40.0

--- upgrade.sh
+++ upgrade.sh
@@ -44,9 +44,3 @@ rm -rf ~/.eo
 sed -i "s|eo.version>[0-9]\+.[0-9]\+.[0-9]\+|eo.version>${version}|" make/jvm/pom.xml
 
 ./pull.sh objectionary/eo
-./pull.sh objectionary/eo-files
-./pull.sh objectionary/eo-strings
-./pull.sh objectionary/eo-collections
-./pull.sh objectionary/eo-sys
-./pull.sh objectionary/eo-hamcrest
-./pull.sh objectionary/eo-math
