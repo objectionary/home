2c86cc57dddd54dedeb525d2bc7f1b8f14de9a4a
Yegor Bugayenko <yegor256@gmail.com>
2024-11-20 08:06:17 +0300

    restore

--- py/deps_tests.py
+++ py/deps_tests.py
@@ -20,9 +20,30 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-all:
-	make -C make
-	python3 py/deps_tests.py
+import unittest
+from unittest.mock import patch
+import sys
+sys.path.insert(0, './py/')
+from deps import dependencies
 
-clean:
-	make -C make clean
+class AutoPullTest(unittest.TestCase):
+    mock_file_content = """
+    +rt jvm org.eolang:eo-runtime:0.29.5
+    +rt jvm org.eolang:eo-files:0.4.0
+    +rt jvm org.eolang:eo-hamcrest:0.4.0
+    """
+
+    @unittest.mock.patch(
+        'builtins.open',
+        new=unittest.mock.mock_open(read_data=mock_file_content),
+        create=True
+    )
+    def test(self):
+        self.assertEqual(
+            dependencies(),
+            {'eo:0.29.5', 'eo-hamcrest:0.4.0', 'eo-files:0.4.0'}
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
